de2601ae404ebb06d5a4db6ff0c1d62dfb9d04a9
Colin Adams <colinpauladams@gmail.com>
2016-03-23 16:24:54 +0000

    Added prototype for C_start_daemon_with_options.

--- src/lmh.h
+++ src/lmh.h
@@ -8,6 +8,12 @@
 struct MHD_Daemon * C_start_daemon (unsigned int flags, uint16_t port, MHD_AcceptPolicyCallback apc, void * apc_cls,
 				    MHD_AccessHandlerCallback dh, void * dh_cls);
 
+struct MHD_Daemon * C_start_daemon_with_options (unsigned int flags, uint16_t port, MHD_AcceptPolicyCallback apc, void * apc_cls,
+						 MHD_AccessHandlerCallback dh, void * dh_cls, size_t connection_memory_limit,
+						 unsigned int connect_limit, unsigned int connect_timeout, unsigned int ip_connection_limit,
+						 unsigned int thread_pool_size, size_t thread_stack_size, unsigned int fastopn_queue_size,
+						 struct MHD_OptionItem ops[]);
+
 struct stat * C_fstat (int fd);
 
 off_t C_file_size (struct stat * sbuf);
